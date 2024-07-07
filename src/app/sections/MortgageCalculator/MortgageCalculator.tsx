a09e2a0246279db99b9809ac24b574ca182a7fc5
Luislev <luis.leiva@utp.ac.pa>
2024-07-06 20:36:43 -0500

    insert calculation logic and handle events into MortgageCalculator component

--- src/app/sections/MortgageCalculator/MortgageCalculator.tsx
+++ src/app/sections/MortgageCalculator/MortgageCalculator.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 import InputForm from "@/app/components/InputForm/InputForm.component";
 import RadioInputForm from "@/app/components/RadioInputform/RadioInputForm";
 import callIcon from "../../assets/images/icon-calculator.svg";
@@ -9,23 +10,93 @@ import "./MortgageCalculator.styles.css";
 
 const MortgageCalculator = () => {
   const mortgageTypeOptions = ["Repayment", "Interest Only"];
-  const isResultAvailable = true;
+
+  const [mortgageData, setmortgageData] = useState({ mortgageAmount: "", mortgageTerm: "", interestRate: "", mortgageType: "" });
+  const [mortgageResult, setMortgageResult] = useState({ monthlyRepayment: "", totalRepayment: "" });
+  const [error, setError] = useState<{ [key: string]: string }>({});
+  const [isResultAvailable, setIsResultAvailable] = useState(false);
+
+  const clearForm = () => {
+    setmortgageData({ mortgageAmount: "", mortgageTerm: "", interestRate: "", mortgageType: "" });
+    setMortgageResult({ monthlyRepayment: "", totalRepayment: "" });
+    setError({});
+    setIsResultAvailable(false);
+  }
+
+  const handleMortgageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value, type } = event.target;
+    if (type === "radio") {
+      setmortgageData({ ...mortgageData, mortgageType: value });
+    } else {
+      setmortgageData({ ...mortgageData, [id]: value });
+    }
+  }
+
+  const validatemortgageData = () => {
+    const errors: { [key: string]: string } = {};
+    if (!mortgageData.mortgageAmount) {
+      errors.mortgageAmount = "This field is required";
+    }
+    if (!mortgageData.mortgageTerm) {
+      errors.interestRate = "This field is required";
+    }
+    if (!mortgageData.interestRate) {
+      errors.mortgageTerm = "This field is required";
+    }
+    if (mortgageData.mortgageType !== "Repayment" && mortgageData.mortgageType !== "Interest Only") {
+      errors.mortgageType = "This field is required";
+    }
+    setError(errors);
+    return Object.keys(errors).length === 0;
+  }
+
+  const calculateMortgageRepayment = () => {
+    const mortgageAmount = parseFloat(mortgageData.mortgageAmount);
+    const mortgageTerm = parseFloat(mortgageData.mortgageTerm);
+    const interestRate = parseFloat(mortgageData.interestRate);
+    const mortgageType = mortgageData.mortgageType;
+
+    let monthlyRepayment = 0;
+    let totalRepayment = 0;
+
+    if (mortgageType === "Repayment") {
+      const monthlyInterestRate = (interestRate / 100) / 12;
+      const totalPayments = mortgageTerm * 12;
+      monthlyRepayment = mortgageAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -totalPayments)));
+      totalRepayment = monthlyRepayment * totalPayments;
+    } else {
+      monthlyRepayment = ((mortgageAmount * interestRate) / 100) / 12;
+      totalRepayment = monthlyRepayment * mortgageTerm * 12;
+    }
+
+    setMortgageResult({ monthlyRepayment: monthlyRepayment.toFixed(2), totalRepayment: totalRepayment.toFixed(2) });
+    setIsResultAvailable(true);
+  };
+
+  const FormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validatemortgageData()) {
+      calculateMortgageRepayment();
+    }
+  }
+
   return (
     <div className="w-full max-w-5xl max-md:mx-0 max-md:my-0 m-8 flex flex-row max-lg:flex-col rounded-3xl shadow-md bg-white">
       <section className="w-full bg-white sm:rounded-t-3xl lg:rounded-l-3xl lg:rounded-r-none">
         <header className="flex justify-between flex-row items-center px-8 py-8 pb-4">
           <h1 className="font-bold text-2xl text-slate900">Mortgage Calculator</h1>
-          <button className="text-slate500 underline text-md">Clear All</button>
+          <button className="text-slate500 underline text-md" onClick={clearForm}>Clear All</button>
         </header>
         <div className="px-8">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={FormSubmit}>
             <InputForm
               id="mortgageAmount"
               type="string"
               label="Mortgage Amount"
               inputSymbol="$"
-              errorMessage=""
-              onChange={() => {}}
+              value={mortgageData.mortgageAmount}
+              errorMessage={error.mortgageAmount}
+              onChange={handleMortgageChange}
             />
             <div className="flex justify-between gap-4 max-sm:gap-1 max-sm:flex-col">
               <InputForm
@@ -33,35 +104,39 @@ const MortgageCalculator = () => {
                 type="string"
                 label="Mortgage Term"
                 inputSymbol="years"
-                errorMessage=""
-                onChange={() => {}}
+                value={mortgageData.mortgageTerm}
+                errorMessage={error.mortgageTerm}
+                onChange={handleMortgageChange}
               />
               <InputForm
                 id="interestRate"
                 type="string"
                 label="Interest Rate"
                 inputSymbol="%"
-                errorMessage=""
-                onChange={() => {}}
+                value={mortgageData.interestRate}
+                errorMessage={error.interestRate}
+                onChange={handleMortgageChange}
               />
             </div>
-            <div className="mt-4">
-              <label className="cursor-pointer-text-slate700">Mortgage Type</label>
+            <div className="mt-2">
+              <label className="cursor-pointer text-slate700">Mortgage Type</label>
               <div className="flex flex-col gap-2 mt-2">
                 {mortgageTypeOptions.map((option, index) => (
                   <RadioInputForm
                     key={index}
                     id={option}
                     label={option}
+                    value={option}
                     name="mortgageType"
-                    isChecked={false}
-                    onChange={() => {}}
+                    isChecked={mortgageData.mortgageType === option}
+                    onChange={handleMortgageChange}
                   />
                 ))}
               </div>
+              {error.mortgageType && <p className="text-red text-sm">{error.mortgageType}</p>}
             </div>
             <div className="w-full flex max-lg:justify-start max-lg:items-center">
-              <div className="my-8 h-full bg-lime py-3 px-8 rounded-full flex flex-wrap gap-4">
+              <div className="my-8 h-full bg-lime py-3 px-8 rounded-full flex flex-wrap justify-center align-center gap-4">
                 <Image src={callIcon} alt="Call Icon" width={24} height={24} />
                 <button
                 type="submit"
@@ -95,7 +170,7 @@ const MortgageCalculator = () => {
                     Your monthly repayment
                   </p>
                   <div className="pb-8 pt-4 border-b-[1px] border-white/10 mb-10">
-                    <h2 className="text-6xl font-bold text-lime">$20.40</h2>
+                      <h2 className="text-6xl font-bold text-lime">${ mortgageResult.monthlyRepayment }</h2>
                   </div>
                 </div>
                 <div>
@@ -103,14 +178,14 @@ const MortgageCalculator = () => {
                     Total you&apos;ll repay over the term
                   </p>
                   <h2 className="font-bold text-2xl text-white my-2">
-                    $24.40
+                    ${mortgageResult.totalRepayment}
                   </h2>
                 </div>
               </div>
             </div>
           </div>
         </div>) :
-          <div className="flex justify-center items-center flex-col max-w-full lg:max-w-[38ch]">
+          <div className="flex justify-center items-center flex-col m-8 max-w-full md:max-w-[55ch] lg:max-w-[38ch]">
           <Image src={emptyResultIcon} alt="Empty Result Icon" width={196} height={196}/>
           <h1 className="font-bold text-2xl text-white w-full text-center mt-2">
             Result shown here
